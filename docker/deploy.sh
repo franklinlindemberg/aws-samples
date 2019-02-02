d30ec5e29b3623838a426f822d2447510ba073d6
Franklin Guimaraes <franklin.lindemberg@gmail.com>
2019-02-02 18:48:03 -0500

    Fixed docker deploy file

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -6,8 +6,7 @@ mkdir tmp/package
 cp -r resources/main tmp/package
 cp -r resources/binaries/. tmp/package
 
-python3 -m pip install --upgrade -i https://test.pypi.org/simple/ cognitive-services --extra-index-url https://pypi.org/simple -t tmp/package
-python3 -m pip install pydub -t tmp/package
+python3 -m pip install -r resources/requirements.txt --upgrade -t tmp/package
 
 echo Creating lambda.zip
 cd tmp/package/
